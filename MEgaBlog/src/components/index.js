32a7fd441fa05fd791deae715e7a3458d40c100f
SaurabhLucifer <saurabhsinghh2r@gmail.com>
2024-02-17 12:30:57 +0530

    First commit

---
+++ MEgaBlog/src/components/index.js
@@ -0,0 +1,30 @@
+import Header from './header/Header';
+import Select from "./Select";
+import Footer from './footer/Footer';
+import Container from "./container/Container";
+import Logo from './Logo';
+import RTE from './RTE';
+import LogoutBtn from './header/LogoutBtn';
+import Signup from './Signup';
+import Button from "./Button";
+import Login from './Login';
+import PostForm from './post-form/PostForm';
+import PostCards from "./PostCards"
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCards,
+    AuthLayout, 
+    Input,
+    Select,
+}
---
+++ MEgaBlog/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login as LoginComponenent } from '../components'
+function Login() {
+  return (
+    <div className='py-8'>
+        <LoginComponenent />
+    </div>
+  )
+}
+
+export default Login
